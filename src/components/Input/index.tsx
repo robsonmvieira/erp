b6939b8f9e933361c6ad3d6ffb4e59f49ee0a63b
Robson Maia <robsonmvieira@gmail.com>
2020-06-15 13:53:30 -0300

    feat: create input component

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import { Container } from './styles'
+
+const Input: React.FC = () => {
+  return (
+    <Container>
+      <input placeholder="Login" />
+    </Container>
+  )
+}
+
+export default Input
