b89ff57acf3fa8cc1e3a529da3cab78d2da476a5
robson maia <maia@DESKTOP-JUNG15H.localdomain>
2020-06-15 23:50:21 -0300

    feat: creat button component

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,14 @@
+import React, {ButtonHTMLAttributes, Children} from 'react';
+
+import { Container } from './styles';
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {}
+
+
+const Button: React.FC<ButtonProps> = ({children, ...rest}) => {
+  return (
+    <Container type='button' {...rest}>{children}</Container>
+  )
+}
+
+export default Button;
---
+++ src/components/Button/styles.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Container = styled.button`
+  margin-top: 10px;
+  padding: 10px 70px;
+  border-radius: 4px;
+  border: 0;
+  background: #5469C2;
+  color: #fff;
+  width: 410px;
+  text-transform: uppercase;
+  font-weight: 500;
+`;
