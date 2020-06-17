22584bfb66906af2caf9f83461bbaf38aae84535
robson maia <maia@DESKTOP-JUNG15H.localdomain>
2020-06-16 23:28:23 -0300

    feat: update background color

--- src/pages/Admin/index.tsx
+++ src/pages/Admin/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import { Container } from './styles';
 
 const Admin: React.FC = () => {
   return (
-    <div>
-      <h1>Tela do Admin</h1>
-    </div>
+    <Container>
+      <h1>Admin</h1>
+    </Container>
     )
 }
 
--- src/pages/Admin/styles.ts
+++ src/pages/Admin/styles.ts
@@ -1,5 +1,7 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  
+  width: 100%;
+  height: 100vh;
+  background-image: linear-gradient(to right, #DBDFFC, #F7D4EE)
 `;
