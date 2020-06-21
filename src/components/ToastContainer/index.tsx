fbb93e617bc184abc2c540746e3f5b8b41d15c23
Robson Maia <robsonmvieira@gmail.com>
2020-06-20 23:45:26 -0300

    feat: upate animanted component

--- src/components/ToastContainer/index.tsx
+++ src/components/ToastContainer/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import { useTransition } from 'react-spring'
 import { Container } from './styles'
 import { ToastMessageProps } from '../../state/toast/state'
 import Toast from './Toast'
@@ -9,10 +9,19 @@ interface ToastContainerProps {
 }
 
 const ToastContainer: React.FC<ToastContainerProps> = ({ messages }) => {
+  const messagesWithTransictions = useTransition(
+    messages,
+    (message) => message.id,
+    {
+      from: { right: '-120%' },
+      enter: { right: '0%' },
+      leave: { right: '-120%' }
+    }
+  )
   return (
     <Container>
-      {messages.map((toast) => (
-        <Toast key={toast.id} message={toast} />
+      {messagesWithTransictions.map(({ item, key, props }) => (
+        <Toast key={key} style={props} message={item} />
       ))}
     </Container>
   )
