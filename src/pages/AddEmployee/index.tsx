54a26d8c2972421fd01cfe93fd5ce96a995792f5
Robson Maia <robsonmvieira@gmail.com>
2020-06-26 19:45:01 -0300

    feat: add form to new employee page

--- src/pages/AddEmployee/index.tsx
+++ src/pages/AddEmployee/index.tsx
@@ -1,23 +1,72 @@
-import React, { useCallback } from 'react'
+import React, { useCallback, useRef } from 'react'
 
-import { Form } from '@unform/core'
-import { Container, Content } from './styles'
+import { Form, FormHandles } from '@unform/core'
+import { Container, Content, MyForm } from './styles'
 import Input from '../../components/Input'
 
 const AddEmployee: React.FC = () => {
-  const handlSubmitForm = useCallback(() => {
-    console.log('send')
+  const formRef = useRef<FormHandles>(null)
+
+  const handlSubmitForm = useCallback((data: any) => {
+    console.log(data)
   }, [])
   return (
     <Container>
       <Content>
-        <Form onSubmit={handlSubmitForm}>
-          <h1>Novo Funcionário</h1>
-          <label htmlFor="">Nome</label>
-          <Input name="employeer-name" />
+        <MyForm ref={formRef} onSubmit={handlSubmitForm}>
+          <div className="form-upload">
+            <input name="employee-img" type="file" />
+          </div>
 
-          <button type="submit">Cadastrar</button>
-        </Form>
+          <div className="container-inputs">
+            <h1>Novo Funcionário</h1>
+            <div className="input-container">
+              <label htmlFor="">Nome</label>
+              <Input format="big" name="employee-name" />
+            </div>
+            <div className="form-group">
+              <div className="input-container">
+                <label htmlFor="">CPF</label>
+                <Input format="big" name="employee-cpf" />
+              </div>
+              <div className="input-container">
+                <label htmlFor="">Identidade</label>
+                <Input format="big" name="employee-civil-id" />
+              </div>
+            </div>
+            <div className="form-group">
+              <div className="input-container">
+                <label htmlFor="">CTPS</label>
+                <Input format="big" name="employee-ctps" />
+              </div>
+              <div className="input-container">
+                <label htmlFor="">Pis</label>
+                <Input format="big" name="employee-pis" />
+              </div>
+            </div>
+            <div className="form-group">
+              <div className="input-container">
+                <label htmlFor="">Email</label>
+                <Input format="big" name="employee-email" />
+              </div>
+              <div className="input-container">
+                <label htmlFor="">Telefone</label>
+                <Input format="big" name="employee-fone" />
+              </div>
+            </div>
+            <div className="input-container">
+              <label htmlFor="">Endereço</label>
+              <Input format="big" name="employee-address" />
+            </div>
+            <div className="input-container">
+              <label htmlFor="">Complemento</label>
+              <Input format="big" name="employee-complement" />
+            </div>
+            <div>
+              <button type="submit">Cadastrar</button>
+            </div>
+          </div>
+        </MyForm>
       </Content>
     </Container>
   )
--- src/pages/AddEmployee/styles.ts
+++ src/pages/AddEmployee/styles.ts
@@ -1,4 +1,75 @@
-import styled from 'styled-components'
+import styled, { keyframes } from 'styled-components'
+import { Form as Unform } from '@unform/web'
 
-export const Container = styled.div``
-export const Content = styled.div``
+export const Container = styled.div`
+  margin: 0 auto;
+  width: 100%;
+`
+
+const fadeIn = keyframes`
+  from {
+    opacity: 0;
+    transform: translateY(-50px)
+  }
+  to {
+    opacity: 1;
+    transform: translateY(0);
+  }`
+export const Content = styled.div`
+  padding: 0 20px;
+  margin: 0 auto;
+  animation: ${fadeIn} 1s;
+
+  & h1 {
+    margin-bottom: 40px;
+  }
+`
+export const MyForm = styled(Unform)`
+  display: flex;
+  width: 100%;
+  justify-content: center;
+
+  & .form-upload {
+    width: 600px;
+    display: flex;
+    align-items: center;
+  }
+
+  & .container-inputs {
+    display: flex;
+    flex-direction: column;
+    flex: 1;
+
+    & .input-container {
+      width: 100%;
+      display: flex;
+      flex-direction: column;
+
+      label {
+        font-family: 'Roboto';
+        font-size: 0.7em;
+        font-weight: normal;
+        margin-bottom: 10px;
+        color: #122232;
+      }
+    }
+
+    & .form-group {
+      width: 100%;
+      display: flex;
+      justify-content: flex-start;
+
+      & .input-container:first-of-type {
+        margin-right: 10px;
+      }
+    }
+    & button {
+      background: #122232;
+      color: #fff;
+      border: none;
+      width: 200px;
+      padding: 10px 15px;
+      border-radius: 5px;
+    }
+  }
+`
