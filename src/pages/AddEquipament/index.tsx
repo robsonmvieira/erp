a8a8d863c1ca19b1e5303a7d445fd0d50a9139bf
Robson Maia <robsonmvieira@gmail.com>
2020-06-29 14:28:22 -0300

    feat: add New Equipament Page

---
+++ src/pages/AddEquipament/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import { Container } from './styles'
+
+const AddEquipament: React.FC = () => {
+  return (
+    <Container>
+      <h2>Equipamento</h2>
+    </Container>
+  )
+}
+
+export default AddEquipament
