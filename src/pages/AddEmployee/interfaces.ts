eb20d037f862ec0ba276bbd1884e9502613da857
Robson Maia <robsonmvieira@gmail.com>
2020-07-01 18:01:38 -0300

    feat: add new fields to interface employee

--- src/pages/AddEmployee/interfaces.ts
+++ src/pages/AddEmployee/interfaces.ts
@@ -8,6 +8,16 @@ export interface Employee {
   fone: string
   address: string
   complement: string
+  adressNumber: string
+  zipCode: string
+  initialSalary: string
+  city: string
+  neighborhood: string
+  stateCode: string
+  hiredDate: string
+  effectiveDate: string
+  asoDate: string
+  hasVehicle: string
 }
 
 export interface Address {
