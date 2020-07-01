0033b76eda7b1c2f3dbc0c3306996a8fce27590f
Robson Maia <robsonmvieira@gmail.com>
2020-07-01 08:58:29 -0300

    feat: move interfaces to other file

---
+++ src/pages/AddEmployee/interfaces.ts
@@ -0,0 +1,18 @@
+export interface Employee {
+  userName: string
+  email: string
+  cpf: string
+  pis: string
+  civilId: string
+  ctps: string
+  fone: string
+  address: string
+  complement: string
+}
+
+export interface Address {
+  logradouro: string
+  bairro: string
+  localidade: string
+  uf: string
+}
